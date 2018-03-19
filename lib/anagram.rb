2be933e899695fb8c2fc6014ef3b241ef3042567
Trevor Seitz <trevinla@gmail.com>
2018-03-19 22:50:42 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -3,7 +3,8 @@ class Anagram
   attr_accessor :word
   def initialize(word)
     @word = word
+    binding.pry
   end
   # alpha_word = word.downcase.chars.sort.join
-  binding.pry
+  # binding.pry
 end
