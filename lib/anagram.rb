3a94f5a858f247984329ee319203e9729db64f97
Trevor Seitz <trevinla@gmail.com>
2018-03-19 22:58:29 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,22 +1,19 @@
 require "pry"
 class Anagram
+
   attr_accessor :word
+
   def initialize(word)
     @word = word
   end
-  # alpha_word = word.downcase.chars.sort.join
 
   def match(word_array)
     anagram_array = []
-    # alpha_word = @word.downcase.chars.sort.join
     word_array.each do |input_word|
-      # alpha_input = input_word.downcase.chars.sort.join
-      # if alpha_input == alpha_word
       if @word.downcase.chars.sort.join == input_word.downcase.chars.sort.join
         anagram_array << input_word
       end
     end
-    # binding.pry
     anagram_array
   end
 
